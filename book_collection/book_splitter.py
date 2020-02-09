6d0712a8af24b16108fe330aaf7c25d8ef5c7979
Jared <clabough.2@buckeyemail.osu.edu>
2020-02-08 23:03:39 -0500

    added the books as pdfs

--- book_collection/book_splitter.py
+++ book_collection/book_splitter.py
@@ -7,6 +7,7 @@ from reportlab.pdfgen import canvas
 
 file_name = str(input())
 input_file = open(file_name, "rb")
+Author = str(input())
 #input_reader = PyPDF2.PdfFileReader(input_file)
 count = 0
 stylesheet = getSampleStyleSheet()
@@ -15,7 +16,7 @@ new_split = input_file.read(50000)
 print(new_split.rfind("\n"))
 split = new_split[0:new_split.rfind("\n")]
 append = new_split[new_split.rfind("\n")]
-while (split) and (count < 60):
+while (split) and (count < 100):
     new_filename = "pdf_collect/" + file_name + str(count) + ".pdf"
     
     #c = canvas.Canvas(new_filename)
@@ -25,7 +26,7 @@ while (split) and (count < 60):
     doc = SimpleDocTemplate(str(new_filename),  \
                         rightMargin=40, leftMargin=40, \
                         topMargin=40, bottomMargin=25, \
-                        pageSize=A4, title=file_name, genre="Fantasy")
+                        pageSize=A4, title=file_name, author=Author)
     
     #doc.pagesize = portrait(A4)
     elements = []
