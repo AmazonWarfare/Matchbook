243980d944db760452e201411455f5b6cac2fa18
Anthony Baietto <36354505+baietto@users.noreply.github.com>
2020-03-05 17:02:51 -0500

    trying firefox

--- test/test_travis.js
+++ test/test_travis.js
@@ -1,15 +1,7 @@
 console.log('######################################################################################################################################################');
 console.log('Beginning Selinium Testing on Travis CI Github');
 
-// Set PATH stuff for Chrome to work
-let chrome = require('selenium-webdriver/chrome');
-let chrome_driver = require('/home/travis/build/AmazonWarfare/Matchbook/node_modules/chromedriver');
-chrome.setDefaultService(new chrome.ServiceBuilder(chrome_driver.path).build());
-
 const {Builder, By, Key, until, Capabilities} = require('selenium-webdriver');
-
-const chromeCapabilities = Capabilities.chrome();
-chromeCapabilities.set('chromeOptions', {args: ['--headless', '--disable-gpu', '--no-sandbox', '--disable-dev-shm-usage', '--disable-setuid-sandbox']});
 
 (async function test_selenium_functioning() {
     let test_state = 0
@@ -18,11 +10,9 @@ chromeCapabilities.set('chromeOptions', {args: ['--headless', '--disable-gpu', '
     console.log('######################################################################################################################################################');
     console.log('Executing test: test_selenium_functioning');
 
-    let driver
+    let driver = await new Builder().forBrowser('firefox').withCapabilities(Capabilities.firefox()).build();
 
     try {
-        driver = chrome.Driver.createSession(chromeCapabilities);
-
         await driver.get('http://www.google.com/ncr');
         await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
         await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
