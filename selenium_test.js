d39982b857a24ec609aad3b015bfb2a5a684ecae
Anthony Baietto <36354505+baietto@users.noreply.github.com>
2020-03-05 17:09:01 -0500

    this should fail

--- selenium_test.js
+++ selenium_test.js
@@ -1,10 +1,7 @@
 console.log('######################################################################################################################################################');
-console.log('Beginning Selinium Testing on Local Machine');
+console.log('Beginning Selinium Testing');
 
 const {Builder, By, Key, until, Capabilities} = require('selenium-webdriver');
-
-const firefox = require('selenium-webdriver/firefox');
-const firefox_driver = require('geckodriver')
 
 (async function test_selenium_functioning() {
     let test_state = 0
@@ -30,6 +27,36 @@ const firefox_driver = require('geckodriver')
 
         if (test_state == 1) {
             console.log('test_selenium_functioning FAILED');
+            console.log('######################################################################################################################################################');
+            throw error
+        }
+    }
+})();
+
+(async function test_selenium_functioning2() {
+    let test_state = 0
+    let error
+
+    console.log('######################################################################################################################################################');
+    console.log('Executing test: test_selenium_functioning2');
+
+    let driver = await new Builder().forBrowser('firefox').withCapabilities(Capabilities.firefox()).build();
+
+    try {
+        await driver.get('http://www.sadklfjsdalfkj.com/ncr');
+        await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+        await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+
+        console.log('test_selenium_functioning2 PASSED');
+        console.log('######################################################################################################################################################');
+    } catch (err) {
+        test_state = 1
+        error = err
+    } finally {
+        await driver.quit();
+
+        if (test_state == 1) {
+            console.log('test_selenium_functioning2 FAILED');
             console.log('######################################################################################################################################################');
             throw error
         }
