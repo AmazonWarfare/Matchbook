f10d13c46d86de53ab32a7b8abe804b02815f9d1
Anthony Baietto <36354505+baietto@users.noreply.github.com>
2020-03-05 09:47:09 -0500

    here I go again

--- test.js
+++ test.js
@@ -1,2 +1,12 @@
-let phantom = require("phantomjs-prebuilt");
-console.log("EUREKA!");
+console.log("EUREKA");
+
+let express = require("express");
+let By = require("selenium-webdriver").By;
+let until = require("selenium-webdriver").until;
+let phantomjs = require("selenium-webdriver/phantomjs");
+let phantomjsnew = require("/usr/local/bin/phantomjs");
+
+let app = express();
+let driver = new phantomjsnew.Driver();
+
+console.log("EUREKA AGAIN");
