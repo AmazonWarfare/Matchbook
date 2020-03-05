b16558b26395d179c2898e137cd8e0ed29286499
Anthony Baietto <36354505+baietto@users.noreply.github.com>
2020-03-05 11:23:12 -0500

    switching from script phantomjs to node

--- test.js
+++ test.js
@@ -1,12 +1,16 @@
 console.log("EUREKA");
 
-/*let express = require("express");
-let By = require("selenium-webdriver").By;
-let until = require("selenium-webdriver").until;
-let phantomjs = require("selenium-webdriver/phantomjs");
-let phantomjsnew = require("/usr/local/bin/phantomjs");
+//let express = require("express");
+//let By = require("selenium-webdriver").By;
+//let until = require("selenium-webdriver").until;
+//let phantomjs = require("selenium-webdriver/phantomjs");
+//let phantomjsnew = require("/usr/local/bin/phantomjs");
 
-let app = express();
-let driver = new phantomjsnew.Driver();
+//let app = express();
+//let driver = new phantomjsnew.Driver();
 
-console.log("EUREKA AGAIN");*/
+let phantomjs = require("/usr/local/bin/phantomjs");
+let test = require("selenium-webdriver");
+console.log(test)
+
+console.log("EUREKA AGAIN");
