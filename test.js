6b261a5a4824831a8f7709af25b9351b62c371f2
Anthony Baietto <36354505+baietto@users.noreply.github.com>
2020-03-05 15:21:34 -0500

    Fixing GitHub not starting Chrome automatically (10)

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+/*
 console.log('MARCO')
 
 // Set PATH stuff for Chrome to work
@@ -9,12 +10,41 @@ chrome.setDefaultService(new chrome.ServiceBuilder(chrome_driver.path).build());
 const {Builder, By, Key, until, Capabilities} = require('selenium-webdriver');
 
 const chromeCapabilities = Capabilities.chrome();
-//chromeCapabilities.set('chromeOptions', {args: ['--headless', '--no-sandbox', '--disable-dev-shm-usage']});
 chromeCapabilities.set('chromeOptions', {args: ['--disable-gpu', '--no-sandbox', '--disable-dev-shm-usage', '--disable-setuid-sandbox']});
 
 (async function example() {
     //let driver = await new Builder().forBrowser('chrome').withCapabilities(chromeCapabilities).build();
     let driver = chrome.Driver.createSession(chromeCapabilities);
+
+    try {
+        await driver.get('http://www.google.com/ncr');
+        await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+        await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+    } finally {
+        await driver.quit();
+    }
+})();
+
+console.log('POLO');
+
+*/
+
+console.log('MARCO')
+
+// Set PATH stuff for Chrome to work
+let chrome = require('selenium-webdriver/chrome');
+//let chrome_driver = require('chromedriver');
+let chrome_driver = require('/home/travis/build/AmazonWarfare/Matchbook/node_modules/chromedriver')
+chrome.setDefaultService(new chrome.ServiceBuilder(chrome_driver.path).build());
+
+const {Builder, By, Key, until, Capabilities} = require('selenium-webdriver');
+
+const chromeCapabilities = Capabilities.chrome();
+chromeCapabilities.set('chromeOptions', {args: ['--disable-gpu', '--no-sandbox', '--disable-dev-shm-usage', '--disable-setuid-sandbox']});
+
+(async function example() {
+    let driver = await new Builder().forBrowser('chrome').withCapabilities(chromeCapabilities).build();
+    //let driver = chrome.Driver.createSession(chromeCapabilities);
 
     try {
         await driver.get('http://www.google.com/ncr');
