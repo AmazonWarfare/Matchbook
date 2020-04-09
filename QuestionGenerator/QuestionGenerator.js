4931f35c9818ee9147c7a19c3548dd46ff567494
srinidhisrinivas <srinidhisrinivas@hotmail.com>
2020-04-09 12:20:33 -0400

    randomized questions for categories and tags

--- QuestionGenerator/QuestionGenerator.js
+++ QuestionGenerator/QuestionGenerator.js
@@ -335,20 +335,14 @@ function QuestionGenerator(){
 
     let generateTernaryCategoryQuestion = function(queryResponse, resultNum){
         let categories = queryResponse.getCategories(resultNum);
-        let foundNewLabel = false;
-        let label;
 
         console.log('Categories:');
         console.log(categories);
 
-        for(let i = 0; i < categories.length; i++){
-            label = categories[i];
-            if(!usedCateg.has(label)){
-                foundNewLabel = true;
-                break;
-            }
-        }
-
+        let remainingCategories = categories.filter(x => !usedCateg.has(x));
+        let foundNewLabel = remainingCategories.length > 0;
+        let label = remainingCategories[Math.floor(Math.random() * remainingCategories.length)];
+       
         if (!foundNewLabel) {
             return 0;
         }
@@ -413,25 +407,20 @@ function QuestionGenerator(){
             3: "\" books?"
         }
         let tags = queryResponse.getTags(resultNum);
-        let foundNewTag = false;
-        let label, tagType;
 
-        for(let i = 0; i < tags.length; i++){
-            label = tags[i].tag_name[0].toUpperCase();
-            tagType = tags[i].tag_type[0];
-            if(!usedTags.has(label)){
-                foundNewTag = true;
-                break;
-            }
-        }
+        console.log(JSON.stringify(tags,null,2));
 
+        let remainingTags = tags.filter(x => !usedTags.has(x.tag_name[0].toUpperCase()));
+        let foundNewTag = remainingTags.length > 0;
+        let tag = remainingTags[Math.floor(Math.random() * remainingTags.length)];
+        
         if (!foundNewTag) {
             return 0;
         }
-        currentLabel = label;
-        currentTagType = tagType;
+        currentLabel = tag.tag_name[0].toUpperCase();
+        currentTagType = tag.tag_type[0];
         formattedLabel = StringFormat.formatDisplayName(currentLabel);
-        usedTags.add(label);
+        usedTags.add(currentLabel);
 
         let question = {
             text: "How do you feel about the concept of \"" + formattedLabel + TAGTYPEQUESTIONMAP[currentTagType],
