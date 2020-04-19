f5dbc9a0a1d110474d33b0dc2d341a06e04cac22
dfclutch <filler.7@osu.edu>
2020-04-19 17:13:23 -0400

    added ProfileForm files and Formik forming package to npm

---
+++ ui/src/components/Inputs/ProfileForm/ProfileForm.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+import "./ProfileForm.scss";
+
+/*
+    Takes 1 Prop:
+
+        this.props.sexual_pref_options: an array of sexual preference options as strings.
+        These are retrieved from the server when form is built
+ */
+class ProfileForm extends Component {
+    render() {
+        return super.render();
+    }
+}
+
+export default ProfileForm;
