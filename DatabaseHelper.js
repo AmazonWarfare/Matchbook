be1584de92c8d9b37df3d31de91eaacc5ef85932
Anthony Baietto <36354505+baietto@users.noreply.github.com>
2020-04-21 18:40:46 -0400

    finished databaseHelper class

--- DatabaseHelper.js
+++ DatabaseHelper.js
@@ -52,9 +52,91 @@ function DatabaseHelper(){
         }
     }
 
-    this.updateUser = async function(userInfo){
+    this.updateUserInfo = async function(userInfo){
+        try {
+            let cursor = collection.find({
+                name: userInfo.name
+            });
+            let cursorArray = cursor.toArray();
 
+            if (cursorArray.length == 0) {
+                await collection.insertOne({
+                    name: userInfo.name,
+                    services: userInfo.services,
+                    gender: userInfo.gender,
+                    sexualPreference: userInfo.sexualPreference,
+                    contactInfo: userInfo.contactInfo
+                });
+            } else {
+                await collection.updateOne(
+                    { name: userInfo.name },
+                    { $set: {
+                            services: cursorArray[0].services.toString().concat(",", userInfo.services),
+                            gender: userInfo.gender,
+                            sexualPreference: userInfo.sexualPreference,
+                            contactInfo: userInfo.contactInfo
+                        }}
+                );
+            }
+        } catch(err) {
+            console.log(err.stack);
+        }
     }
+
+    this.getUserInfo = async function(userInfo){
+        try {
+            let cursor = collection.find({
+                name: userInfo.name
+            });
+            let cursorArray = cursor.toArray();
+
+            if (cursorArray.length == 0) {
+                return null;
+            } else {
+                return {
+                    name: cursorArray[0].name.toString(),
+                    services: cursorArray[0].services.toString(),
+                    gender: cursorArray[0].gender.toString(),
+                    sexualPreference: cursorArray[0].sexualPreference.toString(),
+                    contactInfo: cursorArray[0].contactInfo.toString()
+                }
+            }
+        } catch(err) {
+            console.log(err.stack);
+        }
+    }
+
+    this.getMatchingUsers = async function(userInfo){
+        try {
+            let cursor = collection.find({
+                name: { $ne: userInfo.name },
+                gender: { $in: userInfo.sexualPreference.split(",") },
+                sexualPreference: { $regex : "".concat(".*", userInfo.gender, ".*") }
+            });
+            let cursorArray = cursor.toArray();
+
+            if (cursorArray.length == 0) {
+                return null;
+            } else {
+                let results = [];
+
+                for (let i = 0; i < cursorArray.length; i++) {
+                    results.concat({
+                        name: cursorArray[i].name.toString(),
+                        services: cursorArray[i].services.toString(),
+                        gender: cursorArray[i].gender.toString(),
+                        sexualPreference: cursorArray[i].sexualPreference.toString(),
+                        contactInfo: cursorArray[i].contactInfo.toString()
+                    })
+                }
+
+                return results;
+            }
+        } catch(err) {
+            console.log(err.stack);
+        }
+    }
+
 }
 
 module.exports = DatabaseHelper;
